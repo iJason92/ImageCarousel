c3fdc06e5d705bfe9ea4f25e36dd9110ceb26608
Jason <iJason@163.com>
2015-05-16 20:21:33 +0800

    初始化项目

---
+++ ImageCarousel/ImageCarousel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImageCarousel
+//
+//  Created by Jason on 16/5/15.
+//  Copyright (c) 2015年 Shepherd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
